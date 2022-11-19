3e4051aa888b87aa89d7faaea23f4b6d2e92371c
Herbert Veloso <herbert.oliveira.veloso@gmail.com>
2022-11-18 22:02:27 -0300

    Created websocket

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import http from 'node:http';
 import path from 'node:path';
+import { Server } from 'socket.io';
 
 import { router } from './router';
 
 const port = process.env.PORT || 3001;
 
+const app = express();
+const server = http.createServer(app);
+export const io = new Server(server);
+
 mongoose
   .connect('mongodb://localhost:27017')
   .then(() => {
-    const app = express();
 
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+
+      next();
+    });
     app.use(express.json());
 
     app.use(
@@ -20,7 +32,7 @@ mongoose
 
     app.use(router);
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`🚀 Server is running on http://localhost:${port}`);
     });
   })
